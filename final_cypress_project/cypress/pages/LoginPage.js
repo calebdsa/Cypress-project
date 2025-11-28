cb756cc2173a6bebdd641fa1aca6dc2d972a8d44
calebdsa <55652514+calebdsa@users.noreply.github.com>
2025-11-28 16:39:37 +0530

    Update LoginPage.js

--- final_cypress_project/cypress/pages/LoginPage.js
+++ final_cypress_project/cypress/pages/LoginPage.js
@@ -1,1 +1,19 @@
-class LoginPage{visit(){cy.visit('/login');}};module.exports=new LoginPage();
+class LoginPage {
+  visit() {
+    cy.visit("/index.php?rt=account/login");
+  }
+
+  enterEmail(email) {
+    cy.get("#loginFrm_loginname").type(email);
+  }
+
+  enterPassword(password) {
+    cy.get("#loginFrm_password").type(password);
+  }
+
+  submit() {
+    cy.get("button[title='Login']").click();
+  }
+}
+
+module.exports = new LoginPage();
