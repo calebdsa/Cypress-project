b952a3341d8043afa5f4091993cbfb980ad08975
calebdsa <55652514+calebdsa@users.noreply.github.com>
2025-11-28 16:40:01 +0530

    Update RegisterPage.js

--- final_cypress_project/cypress/pages/RegisterPage.js
+++ final_cypress_project/cypress/pages/RegisterPage.js
@@ -1,1 +1,27 @@
-class RegisterPage{visit(){cy.visit('/register');}};module.exports=new RegisterPage();
+class RegisterPage {
+  visit() {
+    cy.visit("/index.php?rt=account/create");
+  }
+
+  enterFirstName(name) {
+    cy.get("#AccountFrm_firstname").type(name);
+  }
+
+  enterLastName(name) {
+    cy.get("#AccountFrm_lastname").type(name);
+  }
+
+  enterEmail(email) {
+    cy.get("#AccountFrm_email").type(email);
+  }
+
+  enterPassword(password) {
+    cy.get("#AccountFrm_password").type(password);
+  }
+
+  submit() {
+    cy.get("button[title='Continue']").click();
+  }
+}
+
+module.exports = new RegisterPage();
