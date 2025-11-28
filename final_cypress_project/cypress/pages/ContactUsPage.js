cd1b0880dfd6c89fabfb5f7a9c247a2873614117
calebdsa <55652514+calebdsa@users.noreply.github.com>
2025-11-28 16:39:14 +0530

    Update ContactUsPage.js

--- final_cypress_project/cypress/pages/ContactUsPage.js
+++ final_cypress_project/cypress/pages/ContactUsPage.js
@@ -1,1 +1,23 @@
-class ContactUsPage{visit(){cy.visit('/contactus');}};module.exports=new ContactUsPage();
+class ContactUsPage {
+  visit() {
+    cy.visit("/index.php?rt=content/contact");
+  }
+
+  fillName(name) {
+    cy.get("#ContactUsFrm_first_name").type(name);
+  }
+
+  fillEmail(email) {
+    cy.get("#ContactUsFrm_email").type(email);
+  }
+
+  fillEnquiry(msg) {
+    cy.get("#ContactUsFrm_enquiry").type(msg);
+  }
+
+  submit() {
+    cy.get("button[title='Submit']").click();
+  }
+}
+
+module.exports = new ContactUsPage();
