53c01f4c4d882c99a55256bb7505e63955db2506
calebdsa <55652514+calebdsa@users.noreply.github.com>
2025-11-28 16:40:21 +0530

    Update ShoppingPage.js

--- final_cypress_project/cypress/pages/ShoppingPage.js
+++ final_cypress_project/cypress/pages/ShoppingPage.js
@@ -1,1 +1,15 @@
-class ShoppingPage{visit(){cy.visit('/shop');}};module.exports=new ShoppingPage();
+class ShoppingPage {
+  visit() {
+    cy.visit("/index.php?rt=product/category&path=58");
+  }
+
+  selectFirstProduct() {
+    cy.get(".productlist .col-md-3").first().click();
+  }
+
+  addToCart() {
+    cy.get("a[title='Add to Cart']").click();
+  }
+}
+
+module.exports = new ShoppingPage();
